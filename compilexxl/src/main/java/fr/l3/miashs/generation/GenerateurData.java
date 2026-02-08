79eec23362708473aae8b1c4e6e72745fce4df40
Kateryna Babachanakh <katrin.ltvnv@gmail.com>
2026-02-08 17:13:06 +0100

    realisation de classe GenerateurData

--- compilexxl/src/main/java/fr/l3/miashs/generation/GenerateurData.java
+++ compilexxl/src/main/java/fr/l3/miashs/generation/GenerateurData.java
@@ -1,7 +1,32 @@
 package fr.l3.miashs.generation;
 
+import fr.l3.miashs.tds.CategorieSymbole;
+import fr.l3.miashs.tds.Item;
+import fr.l3.miashs.tds.Tds;
+
 /**
- * Classe de génération de données
+ * Génération de la section DATA : variables globales
  */
 public class GenerateurData {
+
+    /**
+     * Génère le code d'initialisation des variables globales à partir de la TDS
+     * @param tds la table des symboles
+     * @return le code d'initialisation des variables globales
+     */
+    public String generer(Tds tds) {
+        StringBuffer sb = new StringBuffer();
+
+        //pour chaque variable de la tds, on génère une ligne de code d'initialisation
+        for (Item item : tds.getItems().values()) {
+
+            //si c une variable GLOBALE
+            if(item.getCategorie() == CategorieSymbole.GLOBAL){
+                int valeur = (item.getVal() != null) ? item.getVal() : 0; //si la variable n'a pas de valeur, on l'initialise à 0
+                sb.append(item.getNom()).append(" : LONG(").append(valeur).append(")\n");
+            }
+
+        }
+        return sb.toString();
+    }
 }
