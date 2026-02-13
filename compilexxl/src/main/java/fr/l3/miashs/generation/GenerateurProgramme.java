65f3e57e2cacf9801142cabcacbed9c461de4225
Kateryna Babachanakh <katrin.ltvnv@gmail.com>
2026-02-13 13:51:24 +0100

    ajout realisation GenerateurProgramme

--- compilexxl/src/main/java/fr/l3/miashs/generation/GenerateurProgramme.java
+++ compilexxl/src/main/java/fr/l3/miashs/generation/GenerateurProgramme.java
@@ -1,5 +1,8 @@
 package fr.l3.miashs.generation;
 
+import fr.l3.miashs.tds.*;
+import fr.ul.miashs.compil.arbre.*;
+
 /**
  * Classe principale du projet, elle utilise les autres classes pour générer un programme complet
  */
@@ -27,5 +30,57 @@ fin
 
  */
 public class GenerateurProgramme {
-    //TODO: à compléter
+
+    /**
+     * Génère le code pour un programme complet
+     * @param a le noeud racine du programme à générer
+     * @param tds la table des symboles
+     * @return le code assembleur complet généré pour le programme
+     */
+    public String generer(Noeud a, Tds tds) {
+
+        // verifications
+        if (a == null) {
+            throw new IllegalArgumentException("Arbre programme null");
+        }
+        if (a.getCat() != Noeud.Categories.PROG) {
+            throw new IllegalArgumentException("Le noeud racine doit être PROG, trouvé: " + a.getCat());
+        }
+        if (tds == null) {
+            throw new IllegalArgumentException("TDS null");
+        }
+
+        StringBuilder code = new StringBuilder();
+
+        // entete du programme
+        code.append(".include beta.uasm\n");
+        code.append("CMOVE(pile, SP)\n");
+        code.append("BR(debut)\n");
+
+        // section DATA
+        code.append(new GenerateurData().generer(tds));
+
+        //point d'entrée
+        code.append("debut:\n");
+        code.append("CALL(main)\n");
+        code.append("HALT()\n");
+
+        // génération de toutes les fonctions
+        GenerateurFonction genFct = new GenerateurFonction();
+        if (a.getFils() != null) {
+            for (Noeud f : a.getFils()) {
+                if (f == null) continue;
+                if (f.getCat() != Noeud.Categories.FONCTION) {
+                    throw new IllegalArgumentException("Fils de PROG non FONCTION: " + f.getCat());
+                }
+                code.append(genFct.generer((Fonction) f, tds));
+            }
+        }
+
+
+        // pile
+        code.append("pile:\n");
+
+        return code.toString();
+    }
 }
