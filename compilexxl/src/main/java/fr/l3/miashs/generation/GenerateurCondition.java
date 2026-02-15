85a17f98ce06f91ed8d281abbff0b8d6754e8d71
Kateryna Babachanakh <katrin.ltvnv@gmail.com>
2026-02-15 18:27:23 +0100

    modif GenerateurCondition

--- compilexxl/src/main/java/fr/l3/miashs/generation/GenerateurCondition.java
+++ compilexxl/src/main/java/fr/l3/miashs/generation/GenerateurCondition.java
@@ -3,41 +3,35 @@ package fr.l3.miashs.generation;
 import fr.ul.miashs.compil.arbre.*;
 import fr.l3.miashs.tds.Tds;
 
+import java.util.List;
+
 /**
  * Classe permettant de générer le code assembleur
  * pour une condition (comparaison)
  */
 
 /*
+pseudo code vu en cours de compilation :
+
 generer_condition(a)
     -> a : arbre
     <- code : ASM
 début
-    code <- generer_expression(fils1(a))
-    code += generer_expression(fils2(a))
-    code += POP(R1)
-    code += POP(R2)
     cas a.categorie de
-        SUP :
+        superieur :
+            code <- generer_expression(fils1(a))
+            code += generer_expression(fils2(a))
+            code += POP(R1)
+            code += POP(R2)
             code += CMPLT(R2, R1, R3)
-        INF :
-            code += CMPLT(R1, R2, R3)
-        SUPE :
-            code += CMPLE(R2, R1, R3)
-        INFE :
-            code += CMPLE(R1, R2, R3)
-        EG :
-            code += CMPEQ(R2, R1, R3)
-        DIF :
-            code += CMPNE(R2, R1, R3)
-    fin cas
-    code += PUSH(R3)
+            сode += PUSH(R3)
+
+       ... autres comparateurs
 fin
 
  */
 public class GenerateurCondition {
 
-    private final StringBuilder out = new StringBuilder();
     private final String scopeFonction;
 
     /**
@@ -55,61 +49,61 @@ public class GenerateurCondition {
      * @return le code assembleur généré
      */
     public String generer(Noeud cond, Tds tds) {
-        out.setLength(0);
-        genererCondition(cond, tds);
-        return out.toString();
-    }
+        if (cond == null) return "";
 
-    /**
-     * Méthode interne de génération de condition
-     */
-    private void genererCondition(Noeud n, Tds tds) {
+        // vérifier que le noeud est bien une condition
+        List<Noeud> fils = cond.getFils();
+        if (fils == null || fils.size() < 2) {
+            throw new IllegalArgumentException("Condition invalide : 2 fils attendus.");
+        }
 
+        StringBuilder code = new StringBuilder();
         GenerateurExpression genExpr = new GenerateurExpression(scopeFonction);
 
         // générer opérande gauche
-        out.append(genExpr.generer(n.getFils().get(0), tds));
+        code.append(genExpr.generer(fils.get(0), tds));
 
         // générer opérande droite
-        out.append(genExpr.generer(n.getFils().get(1), tds));
+        code.append(genExpr.generer(fils.get(1), tds));
 
         // récupérer les deux valeurs
-        out.append("\tPOP(R1)\n");
-        out.append("\tPOP(R2)\n");
+        code.append("\tPOP(R1)\n");
+        code.append("\tPOP(R2)\n");
 
-        switch (n.getCat()) {
+        switch (cond.getCat()) {
 
-            //supérieur
-            case SUP ->
-                    out.append("\tCMPLT(R2, R1, R3)\n"); // R2 > R1
-            //inférieur
-            case INF ->
-                    out.append("\tCMPLT(R1, R2, R3)\n"); // R2 < R1
-            //supérieur ou égal
+            //supérieur (fils_gauche > fils_droite <=> fils_droite < fils_gauche)
+            case SUP -> code.append("\tCMPLT(R1, R2, R3)\n");
+
+            //inférieur (fils_gauche < fils_droite)
+            case INF -> code.append("\tCMPLT(R2, R1, R3)\n");
+
+            //supérieur ou égal (fils_gauche >= fils_droite <=> NOT(fils_gauche < fils_droite))
             case SUPE -> {
-                out.append("\tCMPLT(R2, R1, R3)\n");
-                out.append("\tCMPEQ(R2, R1, R4)\n");
-                out.append("\tADD(R3, R4, R3)\n");
+                code.append("\tCMPLT(R2, R1, R3)\n");// fils_gauche < fils_droite ?
+                code.append("\tCMPEQ(R3, R31, R3)\n");// inverse => fils_gauche >= fils_droite
             }
-            //inférieur ou égal
-            case INFE -> {
-                out.append("\tCMPLT(R1, R2, R3)\n");
-                out.append("\tCMPEQ(R2, R1, R4)\n");
-                out.append("\tADD(R3, R4, R3)\n");
-            }
-            //égal
-            case EG ->
-                    out.append("\tCMPEQ(R2, R1, R3)\n");
-            //différent
-            case DIF ->
-                    out.append("\tCMPNE(R2, R1, R3)\n");
 
-            default ->
-                    throw new UnsupportedOperationException(
-                            "Condition non gérée : " + n.getCat());
+            //inférieur ou égal (fils_gauche <= fils_droite <=> NOT(fils_gauche > fils_droite) <=> NOT(fils_droite < fils_gauche))
+            case INFE -> {
+                code.append("\tCMPLT(R1, R2, R3)\n");   // fils_droite < fils_gauche ? (donc fils_gauche > fils_droite)
+                code.append("\tCMPEQ(R3, R31, R3)\n");  // inverse => fils_gauche <= fils_droite
+            }
+
+            //égal (fils_gauche == fils_droite)
+            case EG -> code.append("\tCMPEQ(R2, R1, R3)\n");
+
+            //différent (fils_gauche != fils_droite <=>  NOT(fils_gauche == fils_droite))
+            case DIF -> {
+                code.append("\tCMPEQ(R2, R1, R3)\n"); // R3 = (R2==R1)
+                code.append("\tCMPEQ(R3, R31, R3)\n"); //R3 = (R3==0) => inverse => (R2!=R1)
+            }
+
+            default -> throw new UnsupportedOperationException("Condition non gérée : " + cond.getCat());
         }
 
-        out.append("\tPUSH(R3)\n");
+        code.append("\tPUSH(R3)\n");
+        return code.toString();
     }
 }
 
