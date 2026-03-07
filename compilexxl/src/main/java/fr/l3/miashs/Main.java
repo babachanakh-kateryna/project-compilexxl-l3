92393e310e1ec5841e5822572b08283a62c6aede
Kateryna Babachanakh <katrin.ltvnv@gmail.com>
2026-03-07 19:09:56 +0100

    main pour run scanner et parser

--- compilexxl/src/main/java/fr/l3/miashs/Main.java
+++ compilexxl/src/main/java/fr/l3/miashs/Main.java
@@ -1,19 +1,30 @@
 package fr.l3.miashs;
+
 import java.io.FileReader;
 import generated.fr.l3.miashs.ParserCup;
 import generated.fr.l3.miashs.Yylex;
+
+
 public class Main {
 
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage : java fr.l3.miashs.Main <fichier_source>");
+            System.exit(1);
+        }
 
-    public static void main(String[] args) throws Exception {
+        try {
+            Yylex scanner = new Yylex(new FileReader(args[0]));
+            ParserCup parser = new ParserCup(scanner);
 
+            parser.parse();
 
-       Yylex scanner = new Yylex(new FileReader(args[0]));
+            System.out.println("OK !");
+        } catch (Exception e) {
+            System.err.println("Erreur pendant l'analyse du fichier : " + args[0]);
+            e.printStackTrace();
+        }
 
-        ParserCup parser = new ParserCup(scanner);
-
-        parser.parse();
-
-        System.out.println("OK !");
+        System.out.println("Terminé !");
     }
 }
