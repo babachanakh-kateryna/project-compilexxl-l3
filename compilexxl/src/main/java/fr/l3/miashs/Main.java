88df6bd5b1f693572e062aba321c5ecc0a4e3cc5
Kateryna Babachanakh <katrin.ltvnv@gmail.com>
2026-03-10 13:34:55 +0100

    ajout generation d un assembleur

--- compilexxl/src/main/java/fr/l3/miashs/Main.java
+++ compilexxl/src/main/java/fr/l3/miashs/Main.java
@@ -1,6 +1,8 @@
 package fr.l3.miashs;
 
 import java.io.FileReader;
+
+import fr.l3.miashs.generation.GenerateurProgramme;
 import generated.fr.l3.miashs.ParserCup;
 import generated.fr.l3.miashs.Yylex;
 import fr.ul.miashs.compil.arbre.Noeud;
@@ -25,18 +27,31 @@ public class Main {
             System.out.println("OK !");
             System.out.println(args[0] + " analysé avec succès.\n");
 
-            if (resultat instanceof Noeud arbre) {
-                System.out.println("===== ARBRE ABSTRAIT =====");
+            Noeud arbre = null;
+
+            System.out.println("========== ARBRE ABSTRAIT ==========");
+            if (resultat instanceof Noeud) {
+                arbre = (Noeud) resultat;
                 TxtAfficheur.afficher(arbre);
             } else {
                 System.out.println("Aucun arbre abstrait produit.");
             }
 
-
             Tds tds = parser.getTds();
 
-            System.out.println("\n===== TABLE DES SYMBOLES =====");
+            System.out.println("========== TABLE DES SYMBOLES ==========");
             System.out.println(tds);
+
+            if (arbre != null) {
+                GenerateurProgramme genProg = new GenerateurProgramme();
+                String code = genProg.generer(arbre, tds);
+
+                System.out.println("========== CODE GENERE ==========");
+                System.out.println(code);
+            } else {
+                System.out.println("========== CODE GENERE ==========");
+                System.out.println("Impossible de générer le code : arbre abstrait absent.");
+            }
 
 
         } catch (Exception e) {
